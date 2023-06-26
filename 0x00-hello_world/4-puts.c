c4d3786d406b523f76eec13336ecf5f65dc93f4d
Akw3t3y <kelvinbotchway200@gmail.com>
2023-06-26 13:52:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,24 @@
+/*
+ *File: 4-puts.c
+ *Author: Akw3t3y
+ *Description: Prints a string using puts
+ */
+
 #include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
+  /*
+   *Print the string ""Programming is like building a multilingual puzzle"
+   *followed by a new line using the puts function
+   */
+  puts("\"Programming is like building a multilingual puzzle");
 
-  return 0;
+  return (0);
 }
