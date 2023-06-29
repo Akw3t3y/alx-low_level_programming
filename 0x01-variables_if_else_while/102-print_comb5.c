af256c5c389a214e546aebc090cf23bf08689cca
Akw3t3y <kelvinbotchway200@gmail.com>
2023-06-29 04:16:02 -0700

    end

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,20 @@
 int main(void)
 {
 int digit1, digit2;
-for (digit1 = 0; digit1 <= 99; digit1++)
+for (digit1 = 0; digit1 <= 8; digit1++)
 {
-for (digit2 = digit1; digit2 <= 99; digit2++)
+for (digit2 = 0; digit2 <= 9; digit2++)
 {
+if (digit1 == digit2)
+continue;
 putchar((digit1 / 10) + '0');
 putchar((digit1 % 10) + '0');
 putchar(' ');
 putchar((digit2 / 10) + '0');
 putchar((digit2 % 10) + '0');
-if (digit1 != 99 || digit2 != 99)
+if (digit1 != 9 || digit2 != 9)
 {
-putchar(' ');
+putchar(',');
 putchar(' ');
 }
 }
