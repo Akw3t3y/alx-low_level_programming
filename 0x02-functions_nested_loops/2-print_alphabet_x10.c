eddb4e736ef206ed34fd76e032b7fa62ba76a071
Akw3t3y <kelvinbotchway200@gmail.com>
2023-06-29 16:41:05 -0700

    phone

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Prints 10 times the alphabet in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+char letter;
+for (digit = 0; digit < 10; digit++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+}
+putchar('\n');
+return (0);
+}
