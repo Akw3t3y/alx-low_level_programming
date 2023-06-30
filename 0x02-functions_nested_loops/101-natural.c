3a002d0c0a24473123233faf350976fbff985f8f
Akw3t3y <kelvinbotchway200@gmail.com>
2023-06-30 15:33:15 -0700

    fib

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,16 +6,13 @@
  */
 int main(void)
 {
-    int i;
-    int sum = 0;
-
-    for (i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
-    }
-
-    printf("%d\n", sum);
-
-    return 0;
+int i;
+int sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+sum += i;
+}
+printf("%d\n", sum);
+return 0;
 }
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+long int num1 = 1, num2 = 2, next_num;
+printf("%ld, %ld", num1, num2);
+for (i = 3; i <= 50; i++)
+{
+next_num = num1 + num2;
+printf(", %ld", next_num);
+num1 = num2;
+num2 = next_num;
+}
+printf("\n");
+return 0;
+}
