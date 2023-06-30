5ee85dfb9a616d9cf0c6d32a0b3431f2068ba319
Akw3t3y <kelvinbotchway200@gmail.com>
2023-06-30 14:26:23 -0700

    lastdig

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - check the code
  *
@@ -6,9 +7,13 @@
  */
 int main(void)
 {
+  int r;
 print_last_digit(98);
+_putchar('\n');
 print_last_digit(0);
-print_last_digit(-1024);
+_putchar('\n');
+r = print_last_digit(-1024);
+_putchar('0' + r);
 _putchar('\n');
 return (0);
 }
