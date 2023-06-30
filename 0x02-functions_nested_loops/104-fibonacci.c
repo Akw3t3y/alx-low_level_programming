62edb3eba952d6eda350d6939be2ebd88e09677d
Akw3t3y <kelvinbotchway200@gmail.com>
2023-06-30 16:03:03 -0700

    finfib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,18 +11,18 @@ int main(void)
 
 void print_fibonacci_sequence(int n)
 {
-    int term1 = 1;  // First term of the Fibonacci sequence
-    int term2 = 2;  // Second term of the Fibonacci sequence
+    int term1 = 1;  /* First term of the Fibonacci sequence */
+    int term2 = 2;  /* Second term of the Fibonacci sequence */
     int i;
 
-    printf("%d, %d", term1, term2);  // Print the first two terms
+    printf("%d, %d", term1, term2);  /* Print the first two terms */
 
     for (i = 3; i <= n; i++)
     {
-        int nextTerm = term1 + term2;  // Calculate the next term
-        printf(", %d", nextTerm);      // Print the next term
+        int nextTerm = term1 + term2;  /* Calculate the next term */
+        printf(", %d", nextTerm);      /* Print the next term */
 
-        term1 = term2;  // Update the values of term1 and term2
+        term1 = term2;  /* Update the values of term1 and term2 */
         term2 = nextTerm;
     }
 }
