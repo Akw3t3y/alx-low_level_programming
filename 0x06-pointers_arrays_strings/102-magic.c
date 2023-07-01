0f8d47055f8a654c0f182c7a28204ea86ee40a7b
Akw3t3y <kelvinbotchway200@gmail.com>
2023-07-01 13:55:30 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,7 +6,7 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
-*(p + 5) = 98; /* Assign 98 to a[2] using pointer arithmetic */  
+*((int *)(((char *)p) + sizeof(int) * 2)) = 98;  /* Add this line */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
