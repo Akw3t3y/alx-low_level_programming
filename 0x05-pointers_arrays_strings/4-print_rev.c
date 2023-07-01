05a1e541363ef94bcc7db3f17119052c9b8464cf
Akw3t3y <kelvinbotchway200@gmail.com>
2023-07-01 07:33:16 -0700

    printrev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to be printed
+ */
+void print_rev(char *s)
+{
+int length = 0;
+int i;
+/* Calculate the length of the string */
+while (s[length] != '\0')
+length++;
+/* Print the characters in reverse order */
+for (i = length - 1; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
+void print_rev(char *s);
 int _putchar(char c);
-void _puts(char *str);
 #endif /* MAIN_H */
