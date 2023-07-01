9202109076ef0153472c6db690daaa502feb38f5
Akw3t3y <kelvinbotchway200@gmail.com>
2023-07-01 10:20:11 -0700

    ato

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,42 +1,27 @@
 #include "main.h"
 /**
- * _atoi - Convert a string to an integer
- * @s: The string to convert
+ * _atoi - Converts a string to an integer
+ * @s: The string to be converted
  *
  * Return: The converted integer
  */
 int _atoi(char *s)
 {
-int i = 0;
 int sign = 1;
-int result = 0;
-/* Handle leading signs */
-while (s[i] == '-')
-i++;
-/* Handle the sign */
-if (s[i] == '-')
+unsigned int num = 0;
+int started = 0;
+while (*s)
 {
-sign = -1;
-i++;
-}
-else if (s[i] == '+')
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
 {
-i++;
+num = (num * 10) + (*s - '0');
+started = 1;
 }
-/* Convert string to integer */
-while (s[i] >= '0' && s[i] <= '9')
-{
-/* Check for overflow */
-if ((result * 10) + (s[i] - '0') < result)
-{
-/* Return minimum or maximum integer value on overflow */
-if (sign == -1)
-return (-2147483648);
-else
-return (2147483647);
+else if (started)
+break;
+s++;
 }
-result = (result * 10) + (s[i] - '0');
-i++;
-}
-return (result *sign);
+return (num *sign);
 }
