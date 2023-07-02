189d5af8cdfc28097408f21f06478526e3ab88ec
Akw3t3y <kelvinbotchway200@gmail.com>
2023-07-02 09:24:11 -0700

    sdss

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,25 @@
 #include "main.h"
+
 /**
- * print_number - Prints an integer
- * @n: The integer to be printed
- */
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
 void print_number(int n)
 {
-int divisor = 1;
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-while (n / divisor > 9)
-divisor *= 10;
-while (divisor != 0)
-{
-_putchar((n / divisor) % 10 + '0');
-divisor /= 10;
-}
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@ int _putchar(char c);
 char *rot13(char *str);
 char *rot13(char *);
 void print_buffer(char *b, int size);
+void print_number(int n);
 #endif /* MAIN_H */
