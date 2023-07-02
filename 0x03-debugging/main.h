10f22120720d5650c3e67895a1fa114849c9690f
Akw3t3y <kelvinbotchway200@gmail.com>
2023-07-02 09:08:06 -0700

    sds

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-/* Function prototype */
 int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
