4b20c661afb02e7947084f909f49e49e96b6b1e4
Akw3t3y <kelvinbotchway200@gmail.com>
2023-07-02 05:05:55 -0700

    end

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
-
-void print_fibonacci_sequence(int n);
-
+/**
+ * main - block to be executed
+ * Description: computes
+ * Return: 0
+ */
 int main(void)
 {
-    print_fibonacci_sequence(98);
-    printf("\n");
-    return 0;
-}
-
-void print_fibonacci_sequence(int n)
+unsigned long int i;
+unsigned long int bef = 1;
+unsigned long int aft = 2;
+unsigned long int l = 1000000000;
+unsigned long int bef1;
+unsigned long int bef2;
+unsigned long int aft1;
+unsigned long int aft2;
+printf("%lu", bef);
+for (i = 1; i < 91; i++)
 {
-    int term1 = 1;  /* First term of the Fibonacci sequence */
-    int term2 = 2;  /* Second term of the Fibonacci sequence */
-    int i;
-
-    printf("%d, %d", term1, term2);  /* Print the first two terms */
-
-    for (i = 3; i <= n; i++)
-    {
-        int nextTerm = term1 + term2;  /* Calculate the next term */
-        printf(", %d", nextTerm);      /* Print the next term */
-
-        term1 = term2;  /* Update the values of term1 and term2 */
-        term2 = nextTerm;
-    }
+printf(", %lu", aft);
+aft += bef;
+bef = aft - bef;
+}
+bef1 = (bef / l);
+bef2 = (bef % l);
+aft1 = (aft / l);
+aft2 = (aft % l);
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", aft1 + (aft2 / l));
+printf("%lu", aft2 % l);
+aft1 = aft1 + bef1;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
+}
+printf("\n");
+return (0);
 }
